e83198c7dc6d211a117dca1317aef2fef733130a
ElenaVyshniak <138595658+ElenaVyshniak@users.noreply.github.com>
2023-11-17 15:02:22 +0200

    02end

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,15 @@
 import Player from '@vimeo/player';
-// import throttle from 'lodash.throttle';
+import throttle from 'lodash.throttle';
 
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
-// const localStorageKey = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const localStorageKey = 'videoplayer-current-time';
 
-// const getCurrentTime = currentTime => {
-//   const seconds = currentTime.seconds;
-//   localStorage.setItem(localStorageKey, JSON.stringify(seconds));
-// };
+const getCurrentTime = currentTime => {
+  const seconds = currentTime.seconds;
+  localStorage.setItem(localStorageKey, JSON.stringify(seconds));
+};
 
-// player.on('timeupdate', throttle(getCurrentTime, 100));
+player.on('timeupdate', throttle(getCurrentTime, 100));
 
-// player.setCurrentTime(JSON.parse(localStorage.getItem(localStorageKey)) || 0);
+player.setCurrentTime(JSON.parse(localStorage.getItem(localStorageKey)) || 0);
